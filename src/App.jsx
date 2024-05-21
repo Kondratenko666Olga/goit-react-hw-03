3698010e536ceec7e068ebbf7d494ee6efebd943
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-05-21 12:47:17 +0300

    SearchBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,50 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import ContactList from './components/ContactList/ContactList'
 import ContactForm from './components/ContactForm/ContactForm'
 import SearchBox from './components/SearchBox/SearchBox'
-// import contactData from './contact.json';
+import contact from '../contact.json'
 
-function App() {
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+      const savedContacts = localStorage.getItem('contacts');
+      return savedContacts ? JSON.parse(savedContacts) : contact;
+    });
+
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (contact) => {
+      setContacts(prevContacts => [contact, ...prevContacts]);
+  };
+
+  const deleteContact = (id) => {
+      setContacts(prevContacts => prevContacts.filter(contact => contact.id !== id));
+  };
+
+  const handleSearchChange = (e) => {
+      setFilter(e.target.value);
+  };
+
+  const getFilteredContacts = () => {
+      const normalizedFilter = filter.toLowerCase();
+      return contacts.filter(contact =>
+          contact.name.toLowerCase().includes(normalizedFilter)
+      );
+  };
 
   return (
-    <>
       <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <SearchBox/>
-        <ContactList/>
+          <h1>Phonebook</h1>
+          <ContactForm onAdd={addContact} />
+          <SearchBox value={filter} onChange={handleSearchChange} />
+          <ContactList contacts={getFilteredContacts()} onDelete={deleteContact} />
       </div>
-    </>
-  )
-}
+  );
+};
 
 export default App
